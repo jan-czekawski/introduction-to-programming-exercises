477e525dc2e6bab11f468b72a3246b9d574e18e7
Michal <bihone88@gmail.com>
2017-05-19 20:34:43 +0200

    Complete exercises 3 - 7

---
+++ the_basics/ex_3.rb
@@ -0,0 +1,6 @@
+movies = 
+  { rocky: 1976, rambo: 1982, gladiator: 2000,
+    matrix: 1999, shawshank_redemption: 1994 }
+    
+puts movies[:rocky], movies[:rambo], movies[:gladiator], movies[:matrix],
+     movies[:shawshank_redemption]
---
+++ the_basics/ex_4.rb
@@ -0,0 +1,2 @@
+dates = [1976, 1982, 2000, 1999, 1994]
+puts dates[0], dates[1], dates[2], dates[3], dates[4]
