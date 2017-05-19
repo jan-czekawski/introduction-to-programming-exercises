f4cbd7216e3a92fcddb925db15304ef364fb7bd7
Michal <bihone88@gmail.com>
2017-05-19 21:46:32 +0200

    Finish exercises 4 - 6

--- variables/name.rb
+++ variables/name.rb
@@ -1,8 +1,13 @@
-puts "Hello! Please type in your name:"
-name = gets.chomp
+puts "Hello! Please type in your first name:"
+first_name = gets.chomp
+puts "And could you please type in your last name:"
+last_name = gets.chomp
+
+name = "#{first_name} #{last_name}"
 
 puts "Hi, #{name}! It's so nice to meet you!!"
 
 10.times do
   puts name
 end
+
