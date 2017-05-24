da82b649c717cf6bcc6892589f59e27e7d3dc0a7
Michal <bihone88@gmail.com>
2017-05-24 11:40:47 +0200

    Complete exercises 1 - 2

---
+++ arrays/ex_1.rb
@@ -0,0 +1,12 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+def array_contains(array, num)
+  if array.include?(num)
+    puts "#{num} is in fact in the array."
+  else
+    puts "Array does not include #{num}"
+  end
+end
+
+array_contains(arr, number)
