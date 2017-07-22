b0e47c1c47310b187d44a25ff122493e15be6f60
Michal <bihone88@gmail.com>
2017-07-22 19:31:05 +0200

    Practice data serialization

---
+++ serialization/marshal_practice.rb
@@ -0,0 +1,61 @@
+class A
+  def initialize(string, number)
+    @string = string
+    @number = number
+  end
+  
+  def to_s
+    "In A:\n    #{@string}, #{@number}\n"
+  end
+end
+
+class B
+  def initialize(number, a_object)
+    @number = number
+    @a_object = a_object
+  end
+  
+  def to_s
+    "In B: #{@number} \n #{@a_object.to_s}\n"
+  end
+end
+
+class C
+  def initialize(b_object, a_object)
+    @b_object = b_object
+    @a_object = a_object
+  end
+  
+  def to_s
+    "In C:\n #{@a_object} #{@b_object}\n"
+  end
+end
+
+a = A.new("hello world", 5)
+b = B.new(7, a)
+c = C.new(b, a)
+
+
+serial_object = Marshal::dump(c)
+puts Marshal::load(serial_object)
+
+serialized_object = YAML::dump(c)
+# puts serialized_object
+puts YAML::load(serialized_object)
+
+File.open("marshal_print.dat", "w") do |file|
+  (1..10).each do |index|
+    file.print Marshal::dump(A.new("hello world", index))
+  end
+end
+
+array = []
+    # $/=     is an input record separator
+$/="\n"
+File.open("marshal_puts.dat", "r").each do |obj|
+  array << Marshal::load(obj)
+end
+
+puts array
+
+
---
+++ serialization/yaml_practice.rb
@@ -0,0 +1,63 @@
+require "yaml"
+
+class A
+  def initialize(string, number)
+    @string = string
+    @number = number
+  end
+  
+  def to_s
+    "In A:\n    #{@string}, #{@number}\n"
+  end
+end
+
+class B
+  def initialize(number, a_object)
+    @number = number
+    @a_object = a_object
+  end
+  
+  def to_s
+    "In B: #{@number} \n #{@a_object.to_s}\n"
+  end
+end
+
+class C
+  def initialize(b_object, a_object)
+    @b_object = b_object
+    @a_object = a_object
+  end
+  
+  def to_s
+    "In C:\n #{@a_object} #{@b_object}\n"
+  end
+end
+
+a = A.new("hello world", 5)
+b = B.new(7, a)
+c = C.new(b, a)
+
+p c
+puts c
+
+serialized_object = YAML::dump(c)
+# puts serialized_object
+puts YAML::load(serialized_object)
+
+File.open("blah_no_space.yaml", "w") do |file|
+  (1..10).each do |index|
+    file.puts YAML::dump(A.new("hello world", index))
+  end
+end
+
+array = []
+#     $/=     is an input record separator
+$/="\n\n"
+File.open("blah.yaml", "r").each do |obj|
+  array << YAML::load(obj)
+end
+
+puts array
+
+
+
