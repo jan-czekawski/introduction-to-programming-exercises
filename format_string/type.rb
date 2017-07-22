3920e51a4e7dfaf12e4217a785d8a1447449cfcb
Michal <bihone88@gmail.com>
2017-07-22 18:28:29 +0200

    Practice Kernel#format method

--- format_string/type.rb
+++ format_string/type.rb
@@ -1,3 +1,4 @@
+=begin
 # INTEGERS
 str = 123
 # binary
@@ -122,4 +123,117 @@ puts format("%x", str)
 puts format("%+x", str)
 
 str = -12
-# puts format("%+e", str)
+puts format("%+e", str)
+
+# width with justify
+str = -1212
+puts format("%-20e", str)
+puts format("%20e", str)
+str = 1000
+puts format("%20d", str)
+puts format("%-20d", str)
+
+
+# pad with 0
+str = 12
+puts format("%020d", str)
+puts format("%020x", str)
+puts format("%020b", str)
+str = -12
+puts format("%020d", str)
+puts format("%020X", str)
+puts format("%020B", str)
+
+# use NEXT arg (next after format string) for width
+str2 = 30
+puts format("%0*d", str2, str)
+puts format("%*d", str2, str)
+
+# # forces to show decimal point
+puts format("%.0e", 1)
+puts format("%#.0e", 1)
+
+puts sprintf("%.0f", 1234)
+puts sprintf("%#.0f", 1234)
+
+# also disables stripping lowest zeros
+puts format("%g", 123.4)
+puts format("%#g", 123.4)
+puts format("%g", 123456)
+puts format("%#g", 123456)
+
+# width
+puts format("%20d", 123)
+puts format("%+20d", 123)
+
+# padding with 0
+puts format("%020d", 123)
+puts format("%+020d", 123)
+puts format("% 020d", 123)
+p format("%-020d", 123)
+p format("%-+20d", 123)
+p format("%- 20d", 123)
+p format("%020x", -123)
+
+=end
+# precision
+
+# min num of digits
+p format("%20.8d", 123)
+p format("%20.8o", 123)
+p format("%-20.8o", 123)
+p format("%20.8x", 123)
+p format("%20.8b", 123)
+p format("%20.8d", -123)
+p format("%20.8o", -123)
+p format("%20.8x", -123)
+p format("%20.8b", -123)
+
+# 0x and 0b are not counted for precision
+# but 0 for #o is counted
+p format("%#20.8d", 123)
+p format("%#20.8o", 123)
+p format("%#20.8x", 123)
+p format("%#20.8b", 123)
+p format("%#20.8d", -123)
+p format("%#20.8o", -123)
+p format("%#20.8x", -123)
+p format("%#20.8b", -11)
+
+# for e precision => num of digs after decimal
+p format("%20.8e", 1234.56789)
+
+# for f precision => num of digs after decimal
+p format("%20.8f", 1234.56789)
+
+# for g => num of significant digits
+p format("%20.8g", 1234.56789)
+p format("%-20.8g", 1234.56789)
+p format("%20.8g", 123456789)
+
+# for s => max num of chars
+p format("%20.8s", "string test")
+p format("%20.20s", "string test")
+
+
+# EXAMPLES
+p format("%d %04x", 123, 123)
+p format("%08b '%4s'", 123, 123)
+p format("%016b '%4s'", 123, 123)
+p format("%1$*2$s %2$d %1$s", "hello", 8)
+p format("%1$*2$s %2$d", "hello", -8)
+p format("%+g:% g:%-g", 1.23, 1.23, 1.23)
+p format("%u", -123)
+
+# reference by name => %<name> is used
+# but %{name} CANNOT USED
+p format("%<foo>d : %<bar>f", { :foo => 1, :bar => 2})
+p format("%<foo>d : %<bar>.2f", { :foo => 1, :bar => 2})
+p format("%<foo>d : %<bar>e", { :foo => 1, :bar => 2})
+# BUT!!!
+p format("%{foo}f", { :foo => 1 })
+
+
+
+
+
