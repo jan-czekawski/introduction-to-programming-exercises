68396afedcbbfadc47d28670efeaeddcf670963b
Michal <bihone88@gmail.com>
2017-07-28 23:48:35 +0200

    Practice exception handling in ruby

---
+++ the_basics/exceptions_handling.rb
@@ -0,0 +1,64 @@
+begin
+  # code that might raise
+rescue
+  # handle exceptions
+end
+
+def my_method
+  # 
+rescue
+  # 
+end
+
+begin
+a = Array.new(2)
+a[0] + a[1]
+rescue => except
+  warn(except.message)
+  # raise
+end
+a[0] = 10 if a[0].nil?
+p a[0]
+p except
+p except.message
+
+begin
+  # 
+rescue ArgumentError, NameError
+  # handle argument or name error
+end
+
+begin
+  # 
+rescue ArgumentError
+  # handle argument error
+rescue NameError
+  # handle name error
+rescue
+  # handle any StandardError
+end
+
+begin
+  # 
+rescue
+  # do sth that may change result of the begin block
+  retry # => might create infinite loop
+end
+
+begin
+  # 
+rescue
+  # 
+ensure
+  puts "regardless of exceptions"
+end
+
+begin
+  # 
+rescue
+  # 
+else
+  puts "no exceptions"
+ensure
+  puts "regardless of exceptions"
+end
