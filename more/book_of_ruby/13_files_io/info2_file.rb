3f18ee1c91d3ac1b913beed73547a40a8043ff11
Michal <bihone88@gmail.com>
2017-08-19 12:43:42 +0200

    Practice sorting and displaying directories in ruby

--- more/book_of_ruby/13_files_io/info2_file.rb
+++ more/book_of_ruby/13_files_io/info2_file.rb
@@ -25,3 +25,19 @@ def process_files(dir)
 end
 
 dir_name = ".."
+
+if !(File.directory?(dir_name))
+  puts "#{dir_name} is not a valid directory"
+else
+  process_files(dir_name)
+  printf("Size of #{dir_name} and subdirectories is #{$dir_size} bytes, #{$dir_size/1024}K, %0.02fMB\n\n", "#{$dir_size/1048576.0}")
+  p "This is an unordered list of the file sizes..."
+  p $files
+  p "This is an unordered list of the file sizes (with 0 byte entries deleted)..."
+  $files.delete(0)
+  p $files
+  p "This is a sorted list (low to high) of the file sizes"
+  p $files.sort
+  p "This is a sorted list (high to low) of the file sizes"
+  p $files.sort.reverse
+end
---
+++ more/book_of_ruby/13_files_io/info3_file.rb
@@ -0,0 +1,42 @@
+$dir_size = 0
+$dirs = {}
+$files = {}
+
+def process_files(dir)
+  total_bytes = 0
+  Dir.foreach(dir) do |f|
+    my_path = "#{dir}/#{f}"
+    
+    if File.directory?(my_path)
+      if f != "." && f != ".."
+        d_size = process_files(my_path) / 1024
+        $dirs[my_path] = d_size
+      end
+    else
+      file_size = File.size(my_path)
+      total_bytes += file_size
+      $files[my_path] = file_size
+    end
+  end
+  
+  $dir_size += total_bytes
+  total_bytes
+end
+
+dir_name = ".."
+
+unless File.directory?(dir_name)
+  puts "#{dir_name} is not a valid directory"
+else
+  process_files(dir_name)
+  printf("Size of this directory and subdirectories is #{$dir_size} bytes, #{$dir_size / 1024}K, %0.02fMB\n\n", "#{$dir_size / 1048576.0}")
+  p "File sizes (ascending)..."
+  $files = $files.sort { |a, b| a[1] <=> b[1] }
+  $files.each { |fname, fsize| p "#{fname} : #{fsize} bytes" }
+  p "Directory sizes (descdending)..."
+  $dirs = $dirs.sort { |a, b| a[1] <=> b[1] }
+  $dirs.each { |dname, dsize| p "#{dname} : #{dsize}K"}
+end
+
+
+
