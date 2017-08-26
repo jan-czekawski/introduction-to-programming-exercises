ebd26b7f8771dd6a2d6789e8213b6209a504aca5
Michal <bihone88@gmail.com>
2017-08-26 09:42:43 +0200

    Practice ruby eval

---
+++ more/book_of_ruby/20_dynamic_programming/eval_string.rb
@@ -0,0 +1,8 @@
+print "What should I evaluate? "
+exp = gets.chomp  # user enters 2 * 4
+puts eval(exp)
+puts "#{exp}"
+puts "#{2 * 4}"
+
+puts "#{eval(exp)}"
+
---
+++ more/book_of_ruby/20_dynamic_programming/str_eval.rb
@@ -0,0 +1,9 @@
+str = "hello world"
+puts "#{str}"
+puts "#{2 * 10}"
+
+"#{def x(s)
+    puts s.reverse
+   end
+   (1..3).each { x(str) }
+}"
