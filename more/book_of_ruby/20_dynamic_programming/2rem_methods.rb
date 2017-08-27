f60ee2145c2da3e0a57cc4b9d4a0a33b8d2cb946
Michal <bihone88@gmail.com>
2017-08-27 16:09:58 +0200

    Practice dynamic programming

---
+++ more/book_of_ruby/20_dynamic_programming/2rem_methods.rb
@@ -0,0 +1,20 @@
+class Y
+  def some_method
+    puts "Y's some_method"
+  end
+end
+
+class Z < Y
+  def some_method
+    puts "Z's some_method"
+  end
+end
+
+zob = Z.new
+zob.some_method  # Z's method
+
+class Z
+  remove_method(:some_method) # remove some_method from Z class
+end
+
+zob.some_method
