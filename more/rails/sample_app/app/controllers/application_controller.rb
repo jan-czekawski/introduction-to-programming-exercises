588f8d17d65c18c7509f07c8fecafcc53d3c3507
Michal <bihone88@gmail.com>
2017-09-01 19:27:59 +0200

    Add file_title method

--- more/rails/sample_app/app/controllers/application_controller.rb
+++ more/rails/sample_app/app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # helper_method :full_title
+
+  # def full_title(page_title = "")
+  #   base_title = "Ruby on Rails Tutorial Sample App"
+  #   if page_title.empty?
+  #     base_title
+  #   else
+  #     page_title + " | " + base_title
+  #   end
+  # end
+
 
   def hello
     render html: "hello, world!"
