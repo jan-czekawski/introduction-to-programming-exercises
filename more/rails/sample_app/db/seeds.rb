b04ab8fdc90e5d352e47a2de72caefe9a02d8b5d
Michal <bihone88@gmail.com>
2017-09-05 00:46:45 +0200

    Add mailers

--- more/rails/sample_app/db/seeds.rb
+++ more/rails/sample_app/db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name: "Harry Potter",
              email: "harry@hogwarts.com",
              password: "password",
              password_confirmation: "password",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
              
 99.times do |n|
   name = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name: "Harry Potter",
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
