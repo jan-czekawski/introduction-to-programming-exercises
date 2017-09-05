2abcca2fa7cf7b75647873d50211c25278595bb9
Michal <bihone88@gmail.com>
2017-09-05 20:16:40 +0200

    Add integration tests, configure sending email in production

--- more/rails/sample_app/test/integration/users_index_test.rb
+++ more/rails/sample_app/test/integration/users_index_test.rb
@@ -10,20 +10,22 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "index including pagination" do
     log_in_as(@admin)
     get users_path
+    users = assigns(:users)
     assert_template "users/index"
+    
     # assert_select "div.pagination", count: 2 # fails after #where(activated)
-    User.where(activated: true).paginate(page: 1).each do |user|
+    users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
     end
   end
   
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
-    get users_path
+    get users_path #+ "?page=1"
+    users = assigns(:users)
     assert_template "users/index"
     # assert_select "div.pagination", count: 2 # fails after #where(activated: true)
-    first_page_of_users = User.where(activated: true).paginate(page: 1)
-    first_page_of_users.each do |user|
+    users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
       unless user == @admin
         assert_select "a[href=?]", user_path(user), text: "delete"
@@ -33,6 +35,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       delete user_path(@not_admin)
     end
   end
+
   
   test "index as non-admin" do
     log_in_as(@not_admin)
