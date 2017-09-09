cc85f7c640ff8c72384ce1ef73b523670f3094a2
michal <bihone@wp.pl>
2017-09-09 19:51:22 +0200

    Update relationship model

--- more/rails/sample_app/test/models/relationship_test.rb
+++ more/rails/sample_app/test/models/relationship_test.rb
@@ -1,7 +1,22 @@
-require 'test_helper'
+require "test_helper"
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:jerry).id,
+                                     followed_id: users(:george).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require follower id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require followed id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
--- more/rails/sample_app/test/models/user_test.rb
+++ more/rails/sample_app/test/models/user_test.rb
@@ -1,11 +1,11 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Jerry", email: "jseinfeld@gmail.com",
                      password: "password", password_confirmation: "password")
   end
-  
+
   test "should be valid" do
     assert @user.valid?
   end
@@ -14,25 +14,25 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "      "
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = "   "
     assert_not @user.valid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
-  
+
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept correct email address" do
-    valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn)
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |adr|
       @user.email = adr
       assert @user.valid?, "#{adr.inspect} should be valid"
@@ -40,47 +40,58 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject incorrect email address" do
-    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com foo@bar..com)
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |inv_adr|
       @user.email = inv_adr
       assert_not @user.valid?, "#{inv_adr.inspect} should be invalid"
     end
   end
-  
+
   test "email address should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-  
+
   test "email address should be saves as lower-case" do
     mixed_case_email = "FoO@ExAmPLE.COm"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-  
+
   test "password should be present (non-blank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
-  
+
   test "password should have minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  
+
   test "authenticated? should return false for user with nil remember_digest" do
     assert_not @user.authenticated?(:remember, nil)
   end
-  
+
   test "associated microposts should be destroyed" do
     @user.save
     @user.microposts.create!(content: "Some text")
     assert_difference "Micropost.count", -1 do
       @user.destroy
     end
+  end
+
+  test "should follow and unfollow a user" do
+    jerry = users(:jerry)
+    george = users(:george)
+    assert_not jerry.following?(george)
+    jerry.follow(george)
+    assert jerry.following?(george)
+    assert george.followers.include?(jerry)
+    jerry.unfollow(george)
+    assert_not jerry.following?(george)
   end
 end
