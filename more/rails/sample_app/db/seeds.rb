33e85754c527b5ac7035e0b8b7b7ccd44780c343
michal <bihone@wp.pl>
2017-09-10 09:47:41 +0200

    Add follow/unfollow forms

--- more/rails/sample_app/db/seeds.rb
+++ more/rails/sample_app/db/seeds.rb
@@ -37,8 +37,17 @@ User.create!(name: "Boss nass",
                activated_at: Time.zone.now)
 end
 
+# microposts
 users = User.order(:created_at).take(7)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# create relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
