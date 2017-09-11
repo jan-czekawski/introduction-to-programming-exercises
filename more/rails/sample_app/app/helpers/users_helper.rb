0bebeae1fa3af100ca6fdcd009f5aea5d00b5390
michal <bihone@wp.pl>
2017-09-11 09:08:03 +0200

    Add ajax requests for follow/unfollow

--- more/rails/sample_app/app/helpers/users_helper.rb
+++ more/rails/sample_app/app/helpers/users_helper.rb
@@ -3,8 +3,7 @@ module UsersHelper
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     # size = options[:size]
-    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    gravatar_url = "kitten.jpeg"
-    image_tag(gravatar_url, width: 95, alt: user.name, class: "gravatar")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
