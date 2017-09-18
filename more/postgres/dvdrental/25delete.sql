b9dfbda10e0e9bdb0865b7b1fadd509a4cec986b
michal8888 <bihone@wp.pl>
2017-09-18 19:18:20 +0200

    Add comments and fix typos in postgres practice

--- more/postgres/dvdrental/25delete.sql
+++ more/postgres/dvdrental/25delete.sql
@@ -9,12 +9,20 @@ WHERE id = 8;
 
 SELECT * FROM link_tmp;
 
+-- to delete based on condition connected to another table have to use USING
 DELETE
 FROM
  link USING link_tmp
 WHERE
  link.id = link_tmp.id;
 
+
+-- same result WHERE ... = 
+DELETE
+FROM
+ link
+WHERE
+ link.id = (SELECT id FROM link_tmp);
 
 DELETE
 FROM link;
