c0657fc39f25784aa2dab88d21435a9e7d4db5db
michal8888 <bihone@wp.pl>
2017-09-29 08:36:20 +0200

    Add class remove test for mongo

--- more/mongo/users/test/delete_test.js
+++ more/mongo/users/test/delete_test.js
@@ -24,8 +24,27 @@ describe('Deleting a user', () => {
 
   });
 
-  it('class method remove', () => {
+  it('model instance remove ALTERNATIVE SYNTAX', (done) => {
+    joe.remove()
+      .then(() => { 
+        User.findOne({ name: 'Joe'})
+          .then((user) => { 
+            assert(user === null);
+            done();
+        }) 
+      });
+  });
 
+  it('class method remove', (done) => {
+    // remove multiple records at the same time with given criteria
+    User.remove({ name: 'Joe' })
+      .then(() => { 
+        User.findOne({ name: 'Joe'})
+          .then((user) => { 
+            assert(user === null);
+            done();
+        }) 
+      });
   });
 
   it('class method findAndRemove', () => {
