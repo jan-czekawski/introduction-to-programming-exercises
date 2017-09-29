e5f61866f47c04c714c8df132582b5701aef15fd
michal8888 <bihone@wp.pl>
2017-09-29 14:17:38 +0200

    Add test for adding and removing posts (subdocuments) from user model

--- more/mongo/users/test/subdocument_test.js
+++ more/mongo/users/test/subdocument_test.js
@@ -19,4 +19,53 @@ describe('Subdocuments', () => {
         done();
       });
   });
+
+
+  it('can add subdocuments to an existing record', (done) => {
+    // will create record, save it, pull out of db, add post to it, make sure post is saved
+    const joe = new User({
+      name: 'Joe',
+      posts: [] // don't have to do it, but we're being explicit, telling that posts are empty
+    });
+
+    joe.save() // we saved joe
+      .then(() => User.findOne({ name: 'Joe' })) // we fetched joe (to var user)
+      .then((user) => {
+        // to add new record inside subdocument collection
+        user.posts.push({ title: 'New Post' }); // array => .push => add new post by joe
+        return user.save(); // save joe => have to use explicit return to chain another .then
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
+  });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        // could use Array.splice => but it's pain in the ass
+        // will use mongoose's alternative API
+        user.posts[0].remove(); // remove() of the subdocument doesn't save automatically !!!
+        // const post = user.posts[0]; =>  same code but in stages
+        // post.remove( ); 
+
+        return user.save(); // have to explicitly save the document
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+
+  });
+
+
 });
