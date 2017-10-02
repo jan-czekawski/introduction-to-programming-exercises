6074ab3544caa4b45d3eba5d64d224b981e76150
michal8888 <bihone@wp.pl>
2017-10-02 09:12:02 +0200

    Connect mongo to mongoose, test creating new driver by asserting count difference before and after request

--- more/mongo/muber/app.js
+++ more/mongo/muber/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 // based on the info in the request; app will run some particular code (access model, go through controller to mongo etc.)
 const app = express();
+
+mongoose.Promise = global.Promise;
+// connect mongoose with instance of the mongo
+mongoose.connect('mongodb://localhost:/muber');
 
 // request handler (2nd arg is callback function)
 // whenever you get http request with method GET
--- more/mongo/muber/test/controllers/drivers_controller_test.js
+++ more/mongo/muber/test/controllers/drivers_controller_test.js
@@ -1,14 +1,30 @@
 const assert = require('assert');
 const request = require('supertest');
+const mongoose = require('mongoose');
 const app = require('../../app');
+
+// (not requiring it) => workaround, because mocha, mongoose and express don't work together in test env
+// mocha tends to try to require more than once (driver may be tried to be created twice)
+const Driver = mongoose.model('driver');
 
 describe('Drivers controller', () => {
   it('Posts to /api/drivers creates a new driver', done => {
-    request(app)
-      .post('/api/drivers') // they are configurations, .post is faking POST, .send sends sth
-      .send({ email: 'test@test.com' }) // email is required  
-      .end(() => {
-        done();
-      });
+    // run count first
+    Driver.count().then(count => {
+      // only after we got the count we start the request
+      request(app)
+        .post('/api/drivers') // they are configurations, .post is faking POST, .send sends sth
+        .send({ email: 'test@test.com' }) // email is required  
+        .end(() => {
+          
+          Driver.count().then(newCount => {
+            assert(count + 1 === newCount);
+          });
+          done();
+        });
+
+    });
+
+
   });
 });
