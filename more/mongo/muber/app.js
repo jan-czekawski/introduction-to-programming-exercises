6b9d69c707917b555b6886e0bbd80a07a9e37313
michal8888 <bihone@wp.pl>
2017-10-02 08:49:10 +0200

    Add body-parser library

--- more/mongo/muber/app.js
+++ more/mongo/muber/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
 // based on the info in the request; app will run some particular code (access model, go through controller to mongo etc.)
 const app = express();
 
@@ -9,7 +10,8 @@ const app = express();
 
 
 // app.post, app.put, app.delete('')
-
+// APP.USE(BODYPARSER) HAVE TO BE DECLARED BEFORE ROUTES(APP) !!!
+app.use(bodyParser.json()) // .json is functioned (invoked here) => any incoming request are parsed into JSON
 routes(app)
 
 module.exports = app;
--- more/mongo/muber/routes/routes.js
+++ more/mongo/muber/routes/routes.js
@@ -6,4 +6,6 @@ module.exports = (app) => {
   // to the route http://localhost:3050/api
   app.get('/api', DriversController.greeting); // we're not invoking the function, only declaring
 
+  app.post('api/drivers', DriversController.create); // we're not invoking the function, no ()
+
 };
