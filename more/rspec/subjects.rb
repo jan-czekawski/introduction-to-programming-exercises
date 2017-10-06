2d1af5bbb21be8fed940f75e8165af149b9a69f8
Michal <bihone88@gmail.com>
2017-10-06 18:32:08 +0000

    Fix typo in subjects in spec

--- more/rspec/subjects.rb
+++ more/rspec/subjects.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 Rspec.describe Agent, "#status" do
   subject { Agent.create(name: "James Bond") }
   
-  it "returns the agents status" do
+  it "returns the agent's status" do
     expect(subject.status).not_to be "MIA"
   end
 end
