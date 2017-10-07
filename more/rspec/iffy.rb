081cd2c0e2c6076e213068523cb8245e7dc8a8b7
Michal <bihone88@gmail.com>
2017-10-07 10:59:39 +0000

    Add another implementation of readable, but not too dry spec

--- more/rspec/iffy.rb
+++ more/rspec/iffy.rb
@@ -24,3 +24,19 @@ describe Agent, "#print_favorite_gadget" do
 end
 
 
+# good implementation of another test
+# we have all the info this test needs in 1 place and don't need to look for specifics
+# it tells story and is not obscure. Not best strategy for DRY code.
+# But clarity and readability outweigh this little bit of repetitive code by longshot
+# especially in large codebases
+describe Agent, "#current_mission" do
+  
+  it "prints out the agent's current mission status and its objective" do
+    mission_octopussy = Mission.new(name: "Octopussy", objective: "Stop bad white dude")
+    bond = Agent.new(name: "James Bond", status: "Undercover operation",
+                     section: "00", licence_to_kill: true)
+    bond.missions << mission_octopussy
+    expect(bond.current_mission).to eq("Agent Bond is currently engaged in an undercover operation for mission Octopussy which aims to stop bad white dude")
+  end
+
+end
