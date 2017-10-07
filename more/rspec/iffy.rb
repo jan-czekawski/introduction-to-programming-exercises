488f60ab0966dda9b616cf008d1a9fffb9fb11ef
Michal <bihone88@gmail.com>
2017-10-07 10:17:00 +0000

    Add better implementation of the same test

--- more/rspec/iffy.rb
+++ more/rspec/iffy.rb
@@ -13,3 +13,14 @@ describe Agent, "#print_favorite_gadget" do
     expect(agent.print_favorite_gadget).to eq("Commander Bond has a thing for Aston Martin")
   end
 end
+
+# better implementation of the same test
+describe Agent, "#print_favorite_gadget" do
+  it "prints out the agent's name, rank and favorite gadget" do
+    agent = Agent.new(name: "James Bond", rank: "Commander", favorite_gadget: "Aston Martin")
+    
+    expect(agent.print_favorite_gadget).to eq("Commander Bond has a thing for Aston Martins")
+  end
+end
+
+
