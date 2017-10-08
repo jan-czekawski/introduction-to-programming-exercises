d248452207d188a7d51a11b60ca379f846b8a754
Michal <bihone88@gmail.com>
2017-10-08 20:07:54 +0000

    Include spec with result of the inheritance

--- more/rspec/factory_girl/random_rails_app/spec/controllers/secret_service_agents_controller_spec.rb
+++ more/rspec/factory_girl/random_rails_app/spec/controllers/secret_service_agents_controller_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe SecretServiceAgentsController, type: :controller do
     
     expect(response).to redirect_to(some_location)
   end
+  
+  # some_spec.rb
+  it "check attributes of agents" do
+    bond = create(:bond)
+    quatermaster = create(:quatermaster)
+    
+    quatermaster.name # => "Q"
+    quatermaster.skills # => "Inventing gizmos and hacking"
+    quatermaster.licence_to_kill # => false
+    
+    bond.name # => "James Bond"
+    bond.skills # => "Espionage and intelligence"
+    bond.licence_to_kill # => true
+  end
 
 end
