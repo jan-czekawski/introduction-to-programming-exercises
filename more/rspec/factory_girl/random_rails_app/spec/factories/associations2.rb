8c89b805fe57705bc09e51888bca289e065a6230
Michal <bihone88@gmail.com>
2017-10-15 10:41:59 +0000

    Update more complex examples of associations in factory girl

--- more/rspec/factory_girl/random_rails_app/spec/factories/associations2.rb
+++ more/rspec/factory_girl/random_rails_app/spec/factories/associations2.rb
@@ -31,3 +31,17 @@ FactoryGirl.define do
   end
 end
 
+# callbacks are handy in this situation. Now can build gun with or without ammo.
+# Via the hash gun: gun you provided the "cartridge" factory with the
+# necessary info to create association via foreign_key
+
+spy_gun = create(:gun)
+spy_gun.cartridges.length # => 0
+
+spy_gun_with_ammo = create(:gun_with_ammo)
+spy_gun_with_ammo.cartridges.length # => 10
+
+# if you need another magazine size you can pass it in via your transient attr
+
+big_magazine_gun = create(:gun_with_ammo, magazine_size: 20)
+big_magazine_gun.cartridges.length # => 20
