55f0d304307129b2abad7b56ff8b177700314b4e
Michal <bihone88@gmail.com>
2017-11-04 10:18:03 +0000

    Add method #find to Post model in mongoid zips app

--- more/mongo/mongoid/11_dao.rb
+++ more/mongo/mongoid/11_dao.rb
@@ -45,3 +45,47 @@ Zip.all({ :state => "NY" }, { :population => -1 }, 0, 1).first
 # there's name clash with module Zip => instead can generate new model
 # eg => rails g model post; and put all the code in that model file
 # instead of Zip use Post
+
+class Post
+  # convenience method for access to client in console
+  def self.mongo_client
+    Mongoid::Clients.default
+  end
+  
+  # convenience method for access to zips collection
+  def self.collection
+    self.mongo_client['zips']
+  end
+  
+  def self.all(prototype = {}, sort = { :population=> 1 }, offset = 0, limit = 100)
+    #map internal :population term to :pop document term
+    tmp = {} #hash needs to stay in stable order provided
+    sort.each {|k,v| 
+      k = k.to_sym==:population ? :pop : k.to_sym
+      tmp[k] = v  if [:city, :state, :pop].include?(k)
+    }
+    sort=tmp
+
+    #convert to keys and then eliminate any properties not of interest
+    prototype=prototype.symbolize_keys.slice(:city, :state) if !prototype.nil?
+
+    Rails.logger.debug {"getting all zips, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
+
+    result=collection.find(prototype)
+          .projection({_id:true, city:true, state:true, pop:true})
+          .sort(sort)
+          .skip(offset)
+    result=result.limit(limit) if !limit.nil?
+
+    return result
+  end  
+  
+  def self.find(id)
+    Rails.logger.debug { "getting zip #{id}" }
+    
+    doc = collection.find(:_id => id)
+                    .projection({ _id: true, city: true, state: true, pop: true })
+                    .first
+    doc.nil? ? nil : Post.new(doc)
+  end
+end
