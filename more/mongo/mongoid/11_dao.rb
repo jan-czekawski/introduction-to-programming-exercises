987e8af82ecd5d0d73f20d8e254ef5bf9627798f
Michal <bihone88@gmail.com>
2017-11-04 10:07:41 +0000

    Add code for #all to zip model file in mongoid in zips app

--- more/mongo/mongoid/11_dao.rb
+++ more/mongo/mongoid/11_dao.rb
@@ -12,6 +12,26 @@ class Zip
   end
   
   def self.all(prototype = {}, sort = { :population=> 1 }, offset = 0, limit = 100)
+    #map internal :population term to :pop document term
+    tmp = {} #hash needs to stay in stable order provided
+    sort.each {|k,v| 
+      k = k.to_sym==:population ? :pop : k.to_sym
+      tmp[k] = v  if [:city, :state, :pop].include?(k)
+    }
+    sort=tmp
+
+    #convert to keys and then eliminate any properties not of interest
+    prototype=prototype.symbolize_keys.slice(:city, :state) if !prototype.nil?
+
+    Rails.logger.debug {"getting all zips, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
+
+    result=collection.find(prototype)
+          .projection({_id:true, city:true, state:true, pop:true})
+          .sort(sort)
+          .skip(offset)
+    result=result.limit(limit) if !limit.nil?
+
+    return result
   end
 end
 
@@ -21,3 +41,7 @@ Zip.mongo_client[:zips]
 Zip.collection.find.count
 Zip.all.count
 Zip.all({ :state => "NY" }, { :population => -1 }, 0, 1).first
+
+# there's name clash with module Zip => instead can generate new model
+# eg => rails g model post; and put all the code in that model file
+# instead of Zip use Post
