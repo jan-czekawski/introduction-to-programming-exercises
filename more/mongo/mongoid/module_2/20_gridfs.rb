41ad2e1191d3c98b1cd1fb427c70044453bc20ef
Michal <bihone88@gmail.com>
2017-11-06 10:18:26 +0000

    Add code for gridFS for mongo. Exporting files and adding description to the files and importing that description to db

--- more/mongo/mongoid/module_2/20_gridfs.rb
+++ more/mongo/mongoid/module_2/20_gridfs.rb
@@ -28,3 +28,26 @@ c = GridfsLoader.mongo_client
 
 # insert data into our database
 r = c.database.fs.insert_one(grid_file)
+
+# EXPORT FILE from GridFS
+stored_file = c.database.fs.find_one(:_id => BSON::ObjectId('5a002cd57b2c270c6a01fc3d'))
+
+# create file to store that exported data
+os_file2 = File.open("./exported_copy.jpg", "w")
+
+# copy the data from chunks to the file
+stored_file.chunks.reduce([]) { |x, chunk| os_file2 << chunk.data.data }
+
+# add description to the file
+description = {}
+
+description[:filename] = "myfile.jpg"
+description[:content_type] = "image/jpeg"
+description[:metadata] = { :author => "kiran", :topic => "nice spot" }
+
+# add the description
+grid_file = Mongo::Grid::File.new(os_file.read, description)
+grid_file.info
+
+# insert into db with the grid file that has all the properties
+r = c.database.fs.insert_one(grid_file)
