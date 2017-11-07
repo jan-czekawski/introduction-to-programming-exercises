d937bd4cabb5fde3e31a90188c4a73cb7b810fc9
Michal <bihone88@gmail.com>
2017-11-07 10:16:29 +0000

    Add code for GridFs with Rails app - ruby 2.2.0, mongoid 5.0. Addidng content, finding and deleting file from the console

--- more/mongo/mongoid/module_2/21_rails_gridfs.rb
+++ more/mongo/mongoid/module_2/21_rails_gridfs.rb
@@ -19,3 +19,35 @@
 
 # :contents is used in app/models/grid_fs_files.rb in #save
 # File.new will use @contents.read to read the content
+
+# in grid_fs_file
+# in the hash it uses camelCase and in the Grid::File it uses snake_case
+# to make mapping a little easier
+
+
+# in #initialize, we take all the properties from the web form and use them for 
+# inserted data
+# if clause => if hash of information came from GridFS use it, if not (and it came)
+# from Rails use rails hash => to instantiate instance variables
+
+# #save will read the content of the object
+
+# #contents => to show that data was successfully uploaded it will go and find_one
+# and basically return data to the browser
+# array of the chunks is fetched into to the single variable => buffer
+
+
+# #destroy => will delete file from the db, based on the id
+
+
+# add another image in the app => create grid fs file
+
+# in rails console:
+GridFsFile.mongo_client.database.fs.find.first[:_id].to_s
+
+# assign that file to an object
+f = GridFsFile.find("5a0080af7b2c27786f000001")
+pp f
+
+# can delete it
+f.destroy
