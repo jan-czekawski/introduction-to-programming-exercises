683e81277d48c15825af4cfcb73ae90e43b1392a
Michal <bihone88@gmail.com>
2017-11-09 10:19:21 +0000

    Add code for adding, listing and deleting indexes in mongo

--- more/mongo/mongoid/module_2/24_indexes.rb
+++ more/mongo/mongoid/module_2/24_indexes.rb
@@ -15,3 +15,15 @@ pp db[:zips].find(state: "NY").explain
 
 # create multiple indexes
 db[:zips].indexes.create_many([{key: { state: 1 }}, { key: { city: 1 }}])
+
+# to fetch all indexes
+db[:zips].indexes.each { |index| p index }
+
+# that example creates ascending index on the field state
+db[:zips].indexes.create_one({ state: 1 })
+
+# to delete the selected id
+db[:zips].indexes.drop_one("state_1")
+
+# to delete all indexes (user created) => _id is not deleted
+db[:zips].indexes.drop_all
