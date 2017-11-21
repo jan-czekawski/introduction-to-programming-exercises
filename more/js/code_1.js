862c751c1ca692b906c1177f64b61e7aa9f4e944
michal8888 <bihone@wp.pl>
2017-11-21 17:21:21 +0100

    Add function for getting score between 2 players in code challenge 1

--- more/js/code_1.js
+++ more/js/code_1.js
@@ -14,3 +14,16 @@ var mark = {
 // var markHeight = 190;
 // var johnAge = 22;
 // var markAge = 29;
+
+function score(playerA, playerB){
+  var scoreA = playerA.height + playerA.age * 5;
+  var scoreB = playerB.height + playerB.age * 5;
+
+  if (scoreA > scoreB) {
+    console.log(playerA.name + "won this round. His score is " + scoreA);
+  } else if (scoreA < scoreB) {
+    console.log(playerB.name + "won this round. His score is " + scoreB);
+  } else {
+    console.log("It's a draw. Both players had following score: " + scoreA);
+  }
+}
