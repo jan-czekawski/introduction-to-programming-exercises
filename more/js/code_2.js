fad8a4d6712f2ad06356f3eb7d80ebfbf2ea7f47
michal8888 <bihone@wp.pl>
2017-11-21 18:49:36 +0100

    Add loop to check if people are adults or minors in code challenge 2

--- more/js/code_2.js
+++ more/js/code_2.js
@@ -1,8 +1,16 @@
-var years = [1903, 1990, 2003, 1988, 1970, 1954];
+var years = [1903, 1990, 2003, 1988, 2010, 1970, 2015, 1954];
 var arr = [];
 
 for (var i = 0; i < years.length; i++){
   arr[i] = years[i];
 }
 
-console.log(arr);
+console.log("All years: " + arr);
+
+for (var i = 0; i < years.length; i++){
+  if (2017 - years[i] >= 18){
+    console.log("Person born in " + years[i] + " is an adult.");
+  } else {
+    console.log("Person born in " + years[i] + " is a minor.");
+  }
+}
