1613f3dee4db9f153fe50f2398fe00ae7f06252c
michal8888 <bihone@wp.pl>
2017-11-21 17:11:01 +0100

    Change variable type to objects in code challenge 1

--- more/js/code_1.js
+++ more/js/code_1.js
@@ -1,6 +1,16 @@
-var johnHeight = 185;
-var markHeight = 190;
-var johnAge = 22;
-var markAge = 29;
+var john = {
+  age: 22,
+  height: 185,
+  name: "John"
+};
 
+var mark = {
+  age: 29,
+  height: 190,
+  name: "Mark"
+};
 
+// var johnHeight = 185;
+// var markHeight = 190;
+// var johnAge = 22;
+// var markAge = 29;
