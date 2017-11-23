3e0c0d8d8e581d784bfe4a4f7ce6babb3d4a65bb
Michal <bihone88@gmail.com>
2017-11-23 18:57:35 +0000

    Add relationship types and more custom types in mongo movie app

--- more/mongo/mongoid/module_3/32_initialize_app.rb
+++ more/mongo/mongoid/module_3/32_initialize_app.rb
@@ -29,3 +29,42 @@ Actor # person having role in Movie
 Writer # 1 of the authors
 Director # 1 of the dirs
 Movie # core info about the movie
+
+# they all have different kind of relationship with other classes
+
+class Writer
+  # (...)
+  field :name, type: String
+  
+  embeds_one :hometown, as: :locatable, class_name: "Place"
+  has_and_belongs_to_many :movies 
+end
+
+class Movies
+  # (...)
+  field :directors, type: Array
+  field :actors, type: Array
+  
+  embeds_many :roles, class_name: "MovieRole"
+  embeds_many :directors, class_name: "DirectorRef"
+  has_and_belongs_to_many :writers
+  has_one :sequel, class_name: "Movie"
+end
+
+# ANOTHER DOCUMENT MODEL CLASS:
+DirectorRef # => annotated reference to Director
+# annotated => used to cache stable/core director info that the referencing
+# doc/view may need
+MovieRole # => character in Movie played by Actor
+          
+# Relationship types:
+              
+# one:one embedded => Actor -> place_of_birth:Place
+# many:one linked => Director -> residence:Place
+# one:many embedded => Movie <-> roles:MovieRole
+# many:one embedded linked => MovieRole <-> Actor
+# one:one linked => Movie -> sequel_to:Movie
+# many:many => Movie <-> writers:Writer
+
+# Models are keys as relations are associations between 1 model and another
+# in the domain and in the db
