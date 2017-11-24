7c0d249c398f3c31e7506034d84709378eb0866a
michal8888 <bihone@wp.pl>
2017-11-24 18:00:01 +0100

    Add examples for embedding and creating embedded docs in the console in mongo movie app

--- more/mongo/mongoid/module_3/33_relationships.rb
+++ more/mongo/mongoid/module_3/33_relationships.rb
@@ -21,3 +21,22 @@ class Writer
   # (...)
   embeds_one :hometown, as: :locatable, class_name: "Place"
 end
+
+# Polymorphic relationships
+# embedding same doc type in several different parent type
+# Child -> polymporphic
+
+actor = Actor.where(place_of_birth: { :$exists => 0 }).first
+oakland = Place.where(city: "Oakland").first
+actor.create_place_of_birth(oakland.attributes)
+
+pp Actor.collection.find(_id: actor.id).first
+
+writer = Writer.where(hometown: { :$exists => 0 }).first
+pp Writer.collection.find(_id: writer.id).first
+oakland = Place.where(city: "Oakland").first
+writer.create_hometown(oakland.attributes)
+
+pp Writer.collection.find(_id: writer.id).first
+
+# 00:48:09
