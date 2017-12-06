2f331cfbe8e09c3c38dbae93855ddca63ec01f31
Michal <bihone88@gmail.com>
2017-12-06 01:53:19 +0000

    Add examples of annotated links in mongo movie app

--- more/mongo/mongoid/module_3/36_many_to_one_embedded.rb
+++ more/mongo/mongoid/module_3/36_many_to_one_embedded.rb
@@ -11,6 +11,27 @@ class MovieRole
   field :main, type: Mongoid::Boolean
   embedded_in :movie
   # (...)
-  belongs_to :actor, :foreign_key => :_id
+  belongs_to :actor, :foreign_key => :_id # can get info on actor by _id
   # (...)
 end
+
+class Actor
+  include Mongoid::Document
+  field :name, type: String
+  
+  # can write has_many to embedded class => have to write application logic
+  # has_many roles:, class_name: "MovieRole"
+  
+  def roles
+    Movie.where(:"roles._id" => self.id) # where role._id matches specific_actor.id
+         .map { |m| m.roles.where(:_id => self.id).first }
+  end
+  
+end
+
+damon = Actor.where(:name => { :$regex => "Matt Da"}).first
+movie = Movie.where(:"roles._id" => damon.id).first
+role = movie.roles.where(:id => damon.id).first
+pp role.attributes
+
+# 1:11:10
