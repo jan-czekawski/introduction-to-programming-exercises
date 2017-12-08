0cef3a0e2f6433956abef6527dead0f4dd7d5713
Michal <bihone88@gmail.com>
2017-12-08 00:03:46 +0000

    Add example of named scope in mongo movie app

--- more/mongo/mongoid/module_3/41_pluck_scope.rb
+++ more/mongo/mongoid/module_3/41_pluck_scope.rb
@@ -10,4 +10,23 @@ Movie.where(rated: "PG").map { |movie| [movie.title, movie.release_date]}
 # projection => only 2 fields are returned
 Movie.where(rated: "PG").pluck(:title, :release_date)
 
+# using lt with a string
 Movie.where(:title.lt => "A").pluck(:title)
+
+# scope => provides good way to reuse common criteria with more business
+# domain style syntax
+
+# named scopes => are simply criteria defined at class load that are referenced
+# by a provided name
+class Movie
+  # (...)
+  field :year, type: Integer
+  scope :current, ->{ where(:year.gt => Date.current.year - 5) }
+end
+
+Movie.current.where(rated: "R").pluck(:title, :year) 
+
+# can use multiple scopes
+
+# instead of providing criteria in the query => can do it
+# in the code on the db layer
