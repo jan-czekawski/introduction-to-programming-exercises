5d846a6766f5a6d2e03d56d2db92c6e61b3f9a3e
Michal <bihone88@gmail.com>
2018-01-02 16:08:09 +0000

    Add examples of put method in mongo movie app

--- more/mongo/mongoid/module_4/46_http_methods.rb
+++ more/mongo/mongoid/module_4/46_http_methods.rb
@@ -5,3 +5,12 @@
 MoviesWS.post("/movies.json", :body => { :movie => {:id => "54321", :title => "rocky54"}}.to_json, :headers => { "Content-Type" => "application/json"})
 # without: :headers => { "Content-Type" => "application/json"} => incorrect parsing
 
+# PUT is for replacing the data (update)
+# client => issues PUT request, issues request to /movies/54321 URI, provides
+# JSON payload for update and application/json MIME type
+response = MoviesWS.put("/movies/54321.json", :body => { :movie => { :title => "rocky5400", :foo => "bar"}}.to_json, :headers => { "Content-Type" => "application/json"})
+
+# PUT(update) - action
+# PUT expects primary key to be in :id param; if the movie is found, processing
+# continues; builds white-list-checked set of params; supplies values to update
+# method; returns the result doc
