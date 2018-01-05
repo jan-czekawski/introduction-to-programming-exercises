507df0ffce259b3fcbb356124b9e5646d9c9843b
michal8888 <bihone@wp.pl>
2018-01-05 10:50:10 +0100

    Add modification of last-modifed in mongo movie app

--- more/mongo/mongoid/module_4/47_caching.rb
+++ more/mongo/mongoid/module_4/47_caching.rb
@@ -16,3 +16,23 @@ pp ["cache-control", "etag", "last-modified"].map { |h| {h => response.header[h]
 Movie.find("54321").cache_key
 # quick digest
 Digest::MD5.hexdigest(Movie.find("54321").cache_key)
+
+Movie.find("54321").updated_at.httpdate
+
+HTTParty.head("https://third-mongoid-workspace-michal8888.c9users.io/movies/54321.json").headers["Last-Modified"]
+# returns nil
+reload!
+
+HTTParty.head("https://third-mongoid-workspace-michal8888.c9users.io/movies/54321.json").headers["Last-Modified"]
+
+
+# Last-Modifed
+# can control Last-Modified
+Movie.find("54321").updated_at.httpdate
+
+def show
+  headers["ETag"] = Digest::MD5.hexdigest(@movie.cache_key)
+  headers["Last-Modified"] => @movie.updated_at.httpdate
+end
+
+HTTParty.head("https://third-mongoid-workspace-michal8888.c9users.io/movies/54321.json").headers["Last-Modified"]
