28e9c68d41192518e8590739952592ffdcf6b760
michal8888 <bihone@wp.pl>
2018-01-11 13:33:07 +0100

    Add assembly of oauth in new app

--- more/mongo/mongoid/module_4/49_oauth.rb
+++ more/mongo/mongoid/module_4/49_oauth.rb
@@ -14,3 +14,21 @@
 # Users have to share credentials
 # Not secure and intrusive
 # Hard to maintain when you authorize many apps(change password at provider - eg FB - clients need to be updated)
+
+
+# OAuth 2 approach
+# secure - no passwords are exchanged; uses tokens; allows providers to give access to users without any exchange of credentials
+
+# OAuth 2.0 is the next evolution of the OAuth protocol which was originally created in late 2006
+
+# ASSEMBLY
+# Core Setup
+# create new Rails app
+# rails new oauth_movies
+# cd oauth_movies
+
+# add gems
+# mongoid; httparty
+
+# integrate mongoid
+# rails g mongoid:config
