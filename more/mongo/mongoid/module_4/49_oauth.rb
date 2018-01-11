04634d06c659e69831837f3893a4164398f1046f
michal8888 <bihone@wp.pl>
2018-01-11 13:27:55 +0100

    Add basics of OAuth in mongo movie app

---
+++ more/mongo/mongoid/module_4/49_oauth.rb
@@ -0,0 +1,16 @@
+# OAuth 2
+
+# OAuth stands for "Open Authorization"
+# Open standard protocol that provides simple and secure authorization for different
+# types of apps
+
+# Allows providers to give access to users without any exchange of credentials
+
+# Authorization framework that enables apps to get limited access to suer accounts
+# on http service like FB, GitHub, TT etc.
+
+# Problems with Password Approach:
+
+# Users have to share credentials
+# Not secure and intrusive
+# Hard to maintain when you authorize many apps(change password at provider - eg FB - clients need to be updated)
