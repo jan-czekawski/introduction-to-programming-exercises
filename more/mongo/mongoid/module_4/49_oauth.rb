9df5fd9a6590402e13cd7227d1bf571652d6fd33
Michal <bihone88@gmail.com>
2018-01-23 21:19:19 +0000

    Add api movies controller in mongo oauth app

--- more/mongo/mongoid/module_4/49_oauth.rb
+++ more/mongo/mongoid/module_4/49_oauth.rb
@@ -90,6 +90,49 @@ module Api
   end
 end
 
+# movies controller focuses on web services interaction
+module Api
+  class MoviesController < Api::BaseController
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
+    
+    def index
+      respond_with Movie.all
+    end
+    
+    def show
+      respond_with @movie
+    end
+    
+    def create
+      respond_with Movie.create(movie_params)
+    end
+    
+    def update
+      respond_with @movie.update(movie_params)
+    end
+    
+    def destroy
+      respond_with @movie.destroy
+    end
+    
+    private
+      def set_movie
+        @movie = Movie.find(params[:id])
+        
+        rescue Mongoid::Errors::DocumentNotFound => e
+          respond_to do |format|
+            format.json { render json: { msg: "movie[#{params[:id]}] not found"}, status: :not_found }
+          end
+      end
+      
+      def movie_params
+        params.require(:movie).permit(:id, :title)
+      end
+      
+      
+  end
+end
+
 
 response = HTTParty.post("https://third-mongoid-workspace-michal8888.c9users.io/api/movies",
                          :body => { :movie => { :id => "54321", :title => "rocky25" }})
