6119126e12d1222e4852f951de0a97a6f93e3e4b
Michal <bihone88@gmail.com>
2018-01-23 22:05:47 +0000

    Update index view (based on if user is logged in) in oauth mongo app

--- more/mongo/mongoid/module_4/50_devise.rb
+++ more/mongo/mongoid/module_4/50_devise.rb
@@ -19,3 +19,47 @@ config.action_mailer.default_url_options = { host: 'https://third-mongoid-worksp
                                              port: 8080 }
 
 # until action mailer is set up, all the emails will be written in server logs
+
+# User Model Class
+# holds accounts in your service -> what someone is validated when authenticating
+# through standard mean (login etc)
+
+# rails g devise user => will create model class
+# could use different user name (eg admin => rails g devise admin)
+
+# snippet
+class User
+  include Mongoid::Document
+  
+  devise :database_authenticable, :registrable, :recoverable,
+         :rememberable, :trackable, :validatable
+         
+  field :email, type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  # ...
+end
+
+# confirmable and lockable are disabled by default
+
+
+# Devise management
+# Devise manages three (3) primary resources for our user:
+# login sessions - login/logout
+# passwords, and
+# registration data - email, optional fields
+
+# update index.html.erb => adding simple set of links to establish login/logout/signup
+# <% if user_signed_in? %>
+#   <h2>Logged in as: <%= current_user.email %></h2>
+#   <ul>
+#     <li><%= link_to 'Log out', destroy_user_session_path, method: :delete %></li>
+#     <li><%= link_to 'Profile', edit_user_registration_path %></li>
+#   </ul>
+# <% else %>
+#   <h2>Welcome!</h2>
+#   <ul>
+#     <li><%= link_to 'Log in', new_user_session_path %></li>
+#     <li><%= link_to 'Sign up', new_user_registration_path %></li>
+#   </ul>
+# <% end %>
