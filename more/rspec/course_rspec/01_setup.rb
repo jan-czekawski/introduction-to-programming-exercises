342d490f86745879e35a69451ab5056c04079354
Michal <bihone88@gmail.com>
2018-01-25 19:00:16 +0000

    Start setting up rspec

---
+++ more/rspec/course_rspec/01_setup.rb
@@ -0,0 +1,21 @@
+# install ruby (and ruby devkit on windows)
+# install rubymine (if you want => ruby ide)
+# run ruby installer (and extract devkit to folder in windows)
+
+# connect ruby devkit with ruby path
+# highlight ruby devkit folder => rmb => open command window here
+# dk.rb init => enter => initialization
+# dk.rb install => enter => installation
+
+# ruby -v => check if ruby is installed
+# gem -v => version of gem you're using => manager to install etc packages in ruby
+# gem install rspec => install rspec
+# rspec -v => check if rspec is installed
+
+# install rubymine
+# open rubymine
+
+# create new project
+# give it a name (rspectConcept)
+# make directory (src)
+
