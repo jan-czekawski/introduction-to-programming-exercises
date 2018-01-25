bf5698785d774f6f52d6cd0cd0d965f2bcff322e
Michal <bihone88@gmail.com>
2018-01-25 19:22:45 +0000

    Finish first test in rspec

--- more/rspec/course_rspec/01_setup.rb
+++ more/rspec/course_rspec/01_setup.rb
@@ -19,3 +19,33 @@
 # give it a name (rspectConcept)
 # make directory (src)
 
+# create new rspec test template
+# filename => name of the test
+# behaviour name => description of the test
+# it should => name of the part of the test
+
+# created template
+# require "rspec"
+
+describe "My very first rspec test class" do
+  it "should first test of many" do
+    puts "first test passed"
+    # true.should == false
+  end
+
+  it "should first test of many" do
+    puts "first test passed"
+    # true.should == false
+  end
+
+  it "should first test of many" do
+    puts "first test passed"
+    # true.should == false
+  end
+end
+
+# to run it => rmb => run (ctrl shift f8)
+
+# in linux => comment require "rspec" and just type rspec 01_setup.rb to run the test
+
+# can use "set" in the bash to see paths
