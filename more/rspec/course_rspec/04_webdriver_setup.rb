8c182727aeb712948df5c4b3ed41335e6e705d59
Michal <bihone88@gmail.com>
2018-01-26 15:26:34 +0000

    Add #before and #after blocks in rspec practice tests

--- more/rspec/course_rspec/04_webdriver_setup.rb
+++ more/rspec/course_rspec/04_webdriver_setup.rb
@@ -5,30 +5,48 @@
 require "selenium-webdriver"
 
 describe "My zoo site" do
+
+  before(:each) do
+    @driver = Selenium::WebDriver.for(:firefox)
+    @driver.navigate.to("https://playball-michal8888.herokuapp.com/courts") # zoo site is not available
+  end
+
   it "should open browser and click on contact" do
     # create webdriver driver
     
     # we want to use selenium webdriver against firefox browser
     # it should open the browser
     # driver = Selenium::WebDriver.for(:firefox)
-    driver = Selenium::WebDriver.for(:chrome)
+    # driver = Selenium::WebDriver.for(:chrome)
     
     # navigate to some website (our zoo site => http://freelancer.geekngames.com/webapp)
     # driver.navigate.to("https://www.google.com") # zoo site is not available
-    driver.navigate.to("https://playball-michal8888.herokuapp.com/courts") # zoo site is not available
+    # driver.navigate.to("https://playball-michal8888.herokuapp.com/courts") # zoo site is not available
     
     # click on contact link (assume id of the contact link is "contact_link")
     # driver.find_element(:id, "contact_link").click
-    driver.find_element(:id, "loginButton").click
+    @driver.find_element(:id, "loginButton").click
     
     # print out page title
-    puts driver.title
+    puts @driver.title
     
-    puts driver.methods
+    puts @driver.methods
 
     # close the browser
-    driver.quit
+    # driver.quit
+  end
+  
+  it "should open browser and click on about" do
+    @driver.find_element(:id, "signupButton").click
     
+    # print out page title
+    puts @driver.title
+    
+    puts @driver.methods
+  end
+  
+  after(:each) do
+    @driver.quit
   end
 end
 
@@ -37,4 +55,14 @@ end
 # https://github.com/mozilla/geckodriver/releases
 
 # but firefox is not installed on cloud ide (check with local environment)
-# 10:27
+
+# can move first 2 lines to "before" action => will run before each tests
+# can move more lines into "after" action 
+
+# before and after methods are run in particular order with tests
+
+# have to be careful about the scope in before, it and after => use instance vars
+
+# we can add more tests => in "it" block
+
+# before(:each) will be run before each "it" tests => before 1 test
