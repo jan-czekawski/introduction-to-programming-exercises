2d61138d252e8b8996cacf87ee27a968aaeb1dc4
michal8888 <bihone@wp.pl>
2018-01-26 22:22:11 +0100

    Use xpath locators (fullpath and double forward slash) in practice rspec

---
+++ more/rspec/course_rspec/06_xpath.rb
@@ -0,0 +1,38 @@
+# can use xpath to find any section of the webpage
+
+require "selenium-webdriver"
+
+describe "Practice xpath" do
+  
+  before(:all) do
+    @driver = Selenium::WebDriver.for(:firefox)
+    @driver.navigate.to("https://dontyoulikelehane-michal8888.herokuapp.com/")
+  end
+
+  it "do xpath magic" do
+    # print out the text from paragraph
+
+    # example of full absolute path
+    puts @driver.find_element(:xpath, "/html/body/div[3]/p").text
+    # count in square bracket starts from "1" => not from "0"
+
+    # example of using "//"
+    puts @driver.find_element(:xpath, "//body//div[3]/p").text
+    puts @driver.find_element(:xpath, "//body/div[3]/p").text
+    puts @driver.find_element(:xpath, "//div[3]/p").text
+  end
+
+  after(:all) do
+    @driver.quit
+  end
+end
+
+# we want to select some tag => there's hierarchy (parent/child relationship):
+# html => body => div[3] (third div => starts from 1) => p etc.
+
+# "/" => start from the root path (html)
+# "//" => look from anywhere in the browser (will look through more tags => more flexible)
+
+# "/html/body/table/tbody/tr[2]/td[2]/h1"
+# "//table//tr[2]/td[2]/h1" => start anywhere, when you find a table (there's only 1), start anywhere
+# inside of the table (there's only 1 tbody) and find tr[2] etc...
