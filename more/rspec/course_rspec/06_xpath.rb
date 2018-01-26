cb99492f335b78fab1c72f7a7fab3f0f225daa1e
michal8888 <bihone@wp.pl>
2018-01-26 22:36:48 +0100

    Use xpath locators (wildcard and attribute) in rspec practice

--- more/rspec/course_rspec/06_xpath.rb
+++ more/rspec/course_rspec/06_xpath.rb
@@ -16,10 +16,18 @@ describe "Practice xpath" do
     puts @driver.find_element(:xpath, "/html/body/div[3]/p").text
     # count in square bracket starts from "1" => not from "0"
 
-    # example of using "//"
+    # example of using "//" => decendants concept
     puts @driver.find_element(:xpath, "//body//div[3]/p").text
     puts @driver.find_element(:xpath, "//body/div[3]/p").text
     puts @driver.find_element(:xpath, "//div[3]/p").text
+
+    # can use wildcard
+    puts @driver.find_element(:xpath, "//*/*/p").text    
+
+    # can use attribute
+    # when using classes => have to list all of them, not just the one
+    puts @driver.find_element(:xpath, '//div[@class="jumbotron center landingJumbo"]/p').text
+
   end
 
   after(:all) do
@@ -36,3 +44,9 @@ end
 # "/html/body/table/tbody/tr[2]/td[2]/h1"
 # "//table//tr[2]/td[2]/h1" => start anywhere, when you find a table (there's only 1), start anywhere
 # inside of the table (there's only 1 tbody) and find tr[2] etc...
+
+# can find sth by attribute => eg "id" (is attribute and has some value), "class" (same) etc
+
+# IF YOU DON'T HAVE TO => DON'T USE XPATH => USE ID/CLASS LOCATORS
+# BUT IF THE ACCESS TO SECTIONS IS DIFFICULT WITH USING ID/CLASS, THEN USE XPATH 
+# IT'S MORE FLEXIBLE, BUT MORE BRITTLE
