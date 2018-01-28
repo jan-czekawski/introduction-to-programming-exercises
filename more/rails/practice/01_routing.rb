fecd8f7c680aa5b5bb7fc3cfca3166a45168ab58
Michal <bihone88@gmail.com>
2018-01-28 12:56:37 +0000

    Practice routing: scopes, multiple files, root path

--- more/rails/practice/01_routing.rb
+++ more/rails/practice/01_routing.rb
@@ -4,7 +4,30 @@
 # rakeroutes -c static-pages (will show only routes for static_pages controller)
 
 # can use scoping to organize your routes => scope by module; by path, by controller, shallow nesting
+# scope in path
+scope "admin" do
+  resources "employees"
+end
 
+# scope in module => in route (another directory)
+scope module: :admin do
+  resources "employees"
+end
+
+# as: :admin => adds "admin" to the prefix => can use that method
+scope "admin", module: :admin, as: :admin do
+  resources "employees"
+end
+
+# SAME RESULT WITH "NAMESPACE"
+namespace "admin" do
+  resources "employees"
+end
+
+scope controller: :management do
+  get "dashboard"
+  get "performance"
+end
 
 # shallow routes => to avoid unecessary nesting
 
@@ -40,3 +63,24 @@ concern :commentable do
 end
 
 resources :pages, concerns: :commentable
+
+# for concerns to be meaningful there must be multiple resources that utilize the concern
+resources :posts, concerns: %i(commentable)
+resources :blogs do
+  concerns :commentable
+end
+
+# can add route page path with "root" method
+root "application#index"
+same as: get "/", to: "application#index"
+
+# can split route file into multiple files (if it's too big) and use
+# require_relative to import it to main file
+
+# make directory in config => and in config/routes/admin_routes.rb
+AppName::Application.routes.draw do
+# Rails.application.routes.draw do => same result
+  namespace :api do
+    resources :posts
+  end
+end
