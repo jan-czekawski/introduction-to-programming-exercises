e1ce2a4c7523974b4895cfd872d334d9fe84262d
michal8888 <bihone@wp.pl>
2018-01-30 02:05:35 +0100

    Add examples of #expect and test it with rspec

--- more/rspec/course_rspec/11_should_expect.rb
+++ more/rspec/course_rspec/11_should_expect.rb
@@ -6,12 +6,34 @@ describe "to test should and expect" do
     num_two = 10
     total = num_one + num_two
     puts total
-    total.should == 15 # will give deprecation warning
-    total.should be == 15 # will give deprecation warning
+
+    # SHOULD
+    # total.should == 15 # will give deprecation warning
+    # total.should be == 15 # will give deprecation warning
     # total.should == 16 # rets failure
     # total.should >= 16 # rets failure
-    total.should != 16 # OK
+    # total.should be != 16 # OK
     # someobject.should be_true
+
+    # EXPECT
+    # expect(total).to be == 15
+    # expect(total).to eq(15)
+    # expect(total).to equal(16)
+    
+    # puts @driver.title
+    title = @driver.title
+    title.should be == "Don't You Like Lehane?"
+    expect(title).to be == "Don't You Like Lehane?"
+
+  end
+
+  before(:all) do
+    @driver = Selenium::WebDriver.for(:firefox)
+    @driver.navigate.to("https://dontyoulikelehane-michal8888.herokuapp.com/")
+  end
+
+  after(:all) do
+    @driver.quit
   end
 end
 
